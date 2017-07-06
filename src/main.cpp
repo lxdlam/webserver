e3fb2a23b855381db67d1e49f02abcc07514619b
lxdlam <lxdlam@gmail.com>
2017-07-06 10:33:29 +0800

    add src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (C), Ramen
+ * File name: main.cpp
+ * Author: Ramen(lxdlam)
+ * Version: 0.1
+ * Date: 2017/07/06
+ * Description: 主程序
+ */
+#include "../include/common.h"
+
+int main()
+{
+}
